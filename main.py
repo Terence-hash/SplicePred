0b2b5089c780b8d5e4159a49676e0e2796eff15f
Terence-hash <69487748+Terence-hash@users.noreply.github.com>
2022-07-10 09:36:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,30 +14,30 @@ from Source.gene_wam import WAM
 from Source.gene_bayes import BN
 from Source.gene_svm import SVM
 
-
-# 加载模型
+# 加载donor site分类器
 du_len, dd_len = 3, 6
 donor_len = du_len + dd_len
 model_dir = f"./Models/Bayes/donor/u3d6_u15d2/"
 donor_clf = BN(du_len, dd_len, model_dir, "donor", "hillclimb")
 donor_thr = 3.02
-# 加载参数
 donor_clf.load_params()
-
+# 加载acceptor site分类器
 au_len, ad_len = 25, 9
 acceptor_len = au_len + ad_len
 model_dir = f"./Models/Bayes/acceptor/u9d15_u25d9/"
 acceptor_clf = BN(au_len, ad_len, model_dir, "acceptor", "hillclimb")
 acceptor_thr = 3.87
-# 加载参数
 acceptor_clf.load_params()
 
-# 提取基因序列
-dir_path = "./Data_files/raw_data/Testing Set/"
-file_name = os.listdir(dir_path)[1]
-with open(dir_path + file_name, 'r') as f:
-    content = f.readlines()
-    whole_seq = "".join([row.strip() for row in content if not row.startswith('>')])
+# 选择文件
+# dir_path = "./Data_files/raw_data/Testing Set/"
+# file_name = os.listdir(dir_path)[0]
+# with open(dir_path + file_name, 'r') as f:
+#     content = f.readlines()
+# 输入序列
+content = input("请输入基因序列：").split()
+
+whole_seq = "".join([row.strip() for row in content if not row.startswith('>')])
 
 # 信号提取
 signals = []
